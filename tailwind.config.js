c434ef2a9636b105a51cd1d331aaf08a15fc2940
Qubir-ui <qubir29@yandex.ru>
2023-04-09 14:36:52 +0300

    realize adaptive layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+const withMT = require("@material-tailwind/html/utils/withMT");
 module.exports = {
   content: ["./index.html", "./js/script.js"],
   theme: {
